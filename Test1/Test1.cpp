af9adb0fe757c6b5b391d45c14e7db0e575d786a
yzh <yaozihao5606@outlook.com>
2023-02-17 20:31:57 +0800

    添加项目文件。

---
+++ Test1/Test1.cpp
@@ -0,0 +1,10 @@
+﻿# include <stdio.h>
+
+int main() {
+	printf("%llu", sizeof(char));
+	printf("%llu", sizeof(short));
+	printf("%llu", sizeof(int));
+	printf("%llu", sizeof(long));
+	printf("%llu", sizeof(long long));
+	return 0;
+}
